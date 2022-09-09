8a15e53869db2b25791138b5d4fd122b0d4fb269
Claire Cashmore <clairecashmore2017@gmail.com>
2022-09-09 09:07:48 -0400

    finished thoughts and reactions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -58,6 +58,44 @@ const thoughtController = {
       })
       .catch((err) => res.json(err));
   },
+
+  //reaction section
+
+  //add reaction to thought
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      {
+        _id: params.thoughtId,
+      },
+      {
+        $push: { reactions: body },
+      },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        // If there is no data found at the id
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+  //remove a reaction
+  removeReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      {
+        _id: params.thoughtId,
+      },
+      {
+        $pull: { reactions: { reactionId: params.reactionId } },
+      },
+      { new: true }
+    )
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
 };
 
 module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -2,12 +2,17 @@ const router = require("express").Router();
 const {
   addThought,
   deleteThought,
+  addReaction,
+  removeReaction,
 } = require("../../controllers/thought-controller");
 
 //routes for adding thoughts
 router.route("/:userId").post(addThought);
 
 //route for removing thoughts
-router.route("/:userId/:thoughtId").delete(deleteThought);
+router.route("/:userId/:thoughtId").delete(deleteThought).put(addReaction);
+
+//route for removing reactions
+router.route("/:userId/:thoughtId/:reactionId").delete(removeReaction);
 
 module.exports = router;
